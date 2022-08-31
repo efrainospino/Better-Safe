45832e667b1cde295db1e4a158fb5a602281620f
Efrain Ospino <efrainospino14@gmail.com>
2022-08-31 13:42:16 -0500

    cuarto commit

--- pages/api/items.js
+++ pages/api/items.js
@@ -1,55 +1,57 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json([
-    {
-      id:0,
-      title: "Gas Pimienta",
-      price: 17000,
-      image: "https://vercel.com/img/gasPimienta.jpg",
-      description: "Producto aturdidor."
-    },
-    {
-      id:1,
-      title: "Taser",
-      price: 40000,
-      image: "https://vercel.com/img/taser.jpg",
-      description: "Producto de alto voltaje, ideal para la defensa personal y aturdir."
-    },
-    {
-      id:2,
-      title: "Alarma",
-      price: 22000,
-      image: "https://vercel.com/img/alarma.jpg",
-      description: "Producto ideal para captar la atencion de las personas y alejar al delincuente."
-    },
-    {
-      id:3,
-      title: "Kubotan",
-      price: 16000,
-      image: "https://vercel.com/img/kubotan.jpg",
-      description: "Producto corto punzante para defensa personal."
-    },
-    {
-      id:4,
-      title: "Rompevidrios",
-      price: 16000,
-      image: "https://vercel.com/img/rompevidrios.jpg",
-      description: "Martillo de seguridad para romper vidrios y/o cinturones."
-    },
-    {
-      id:5,
-      title: "Kit basico",
-      price: 55000,
-      image: "https://vercel.com/img/kitbasico.jpg",
-      description: "Kit de gas pimienta y Taser."
-    },
-    {
-      id:6,
-      title: "Kit llavero",
-      price: 75000,
-      image: "https://vercel.com/img/llaveroKit.jpg",
-      description: "Kit con gas pimienta, rompevidrios, alarma, kubotan y pompom"
-    },
-  ])
-}
+  if (req.method === 'GET'){
+      res.status(200).json([
+        {
+          id:0,
+          title: "Gas Pimienta",
+          price: 17000,
+          image: "http://localhost:3000/img/gasPimienta.jpg",
+          description: "Producto aturdidor."
+        },
+        {
+          id:1,
+          title: "Taser",
+          price: 40000,
+          image: "http://localhost:3000/img/taser.jpg",
+          description: "Producto de alto voltaje, ideal para la defensa personal y aturdir."
+        },
+        {
+          id:2,
+          title: "Alarma",
+          price: 22000,
+          image: "http://localhost:3000/img/alarma.jpg",
+          description: "Producto ideal para captar la atencion de las personas y alejar al delincuente."
+        },
+        {
+          id:3,
+          title: "Kubotan",
+          price: 16000,
+          image: "http://localhost:3000/img/kubotan.jpg",
+          description: "Producto corto punzante para defensa personal."
+        },
+        {
+          id:4,
+          title: "Rompevidrios",
+          price: 16000,
+          image: "http://localhost:3000/img/rompevidrios.jpg",
+          description: "Martillo de seguridad para romper vidrios y/o cinturones."
+        },
+        {
+          id:5,
+          title: "Kit basico",
+          price: 55000,
+          image: "http://localhost:3000/img/kitbasico.jpg",
+          description: "Kit de gas pimienta y Taser."
+        },
+        {
+          id:6,
+          title: "Kit llavero",
+          price: 75000,
+          image: "http://localhost:3000/img/llaveroKit.jpg",
+          description: "Kit con gas pimienta, rompevidrios, alarma, kubotan y pompom"
+        },
+      ])
+    }
+  }
