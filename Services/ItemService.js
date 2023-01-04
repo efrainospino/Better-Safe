57cbde92f479e02992b3abe8cb6e0817bd867aaa
Efrain Ospino <efrainospino14@gmail.com>
2023-01-03 19:04:08 -0500

    update server

--- Services/ItemService.js
+++ Services/ItemService.js
@@ -1,5 +1,5 @@
 export async function getItems(){
-    const request = await fetch(`https://better-safe.herokuapp.com/productos`);
+    const request = await fetch(`https://productos-bs.up.railway.app/productos`);
     const items = await request.json();
 
     return items;
